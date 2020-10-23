6499d83a3b147acad69d8f86dc1f7017e62854ee
harysuryanto <hary.suryanto01@gmail.com>
2020-10-23 22:19:15 +0700

    add peer and peerjs dependecies

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    secure: true, 
+    // secure: true,
     // host: '/', // for run in local
     // host: 'novelin-zoom-peerjs-server.herokuapp.com',
     // host: 'novelin-peerjs-server.herokuapp.com',
     // port: 57171
     path: '/peerjs',
     host: '/',
-    port: 443
+    port: '443'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
